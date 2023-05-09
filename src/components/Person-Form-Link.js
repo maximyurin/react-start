72e44eea0a0d1a284c69760c1f33c4b852fa64b7
Maxim <maximyurinmusic@gmail.com>
2023-05-09 20:50:12 +0300

    Create branch

--- src/components/Person-Form-Link.js
+++ src/components/Person-Form-Link.js
@@ -1,11 +1,11 @@
 function PersonFormLink() {
-  return (
-    <div className="person-form-link-container">
-      <a href="./hire-me.html" className="form-link">
-        Hire me
-      </a>
-    </div>
-  );
+    return (
+        <div className="person-form-link-container">
+            <a href="./hire-me.html" className="form-link">
+                Hire me
+            </a>
+        </div>
+    );
 }
 
 export default PersonFormLink;
